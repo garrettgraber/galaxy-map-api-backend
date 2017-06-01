bff766d61ea7e40cbeb8cf19953954e0fce8d132
Garrett Graber <garrett_graber@yahoo.com>
2017-05-31 21:19:36 -0600

    Changed ip

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if(DatabaseLinks.hasOwnProperty('tiles') && DatabaseLinks.hasOwnProperty('mongo'
   	var MONGO = 'mongodb://' + DatabaseLinks.mongo.hostname + ':' + DatabaseLinks.mongo.port;
 } else if (isProduction) {
 	var TILES = '';
-	var MONGO = 'http://ec2-54-164-234-120.compute-1.amazonaws.com';
+	var MONGO = 'http://172.31.65.109:27017';
 } else {
   var TILES = 'http://localhost:8110/tiles-leaflet-new/{z}/{x}/{y}.png';
 }
