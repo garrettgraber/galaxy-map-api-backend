0d12b6262f491c8af2932c6b43128d42e7f988f2
garrettgraber <garrett_graber@yahoo.com>
2017-10-10 21:03:13 -0600

    Created daemon shell script

--- docker/run-production.sh
+++ docker/run-production.sh
@@ -6,5 +6,5 @@
 docker rm -f galaxy-map-server
 echo "Running galaxy-map-server.."
 
-docker run --name galaxy-map-server -d --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8103 galaxy-map-server
+docker run --name galaxy-map-server  --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8103 galaxy-map-server
 
