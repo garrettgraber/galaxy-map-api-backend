389c57443322488b1df13d24ad369672bc8336d2
garrettgraber <garrett_graber@yahoo.com>
2025-01-23 00:22:38 -0700

    Removed build

--- docker/run-production-daemon.sh
+++ docker/run-production-daemon.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-./build.sh
+# ./build.sh
 # echo "Deleting "
 docker rm -f galaxy-map-server
 echo "Running galaxy-map-server.."
 
-docker run --name galaxy-map-server -d --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8103 galaxy-map-server
-
+docker run --name galaxy-map-server -d --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8103 gmoneycool/galaxy-map-server
