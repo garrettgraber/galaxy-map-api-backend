69026cea51335d861608fa10a470ec46dc351b4b
garrettgraber <garrett_graber@yahoo.com>
2025-05-31 15:02:47 -0600

    Changed nav com address.

--- docker/run-dev.sh
+++ docker/run-dev.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-./build.sh
+# ./build.sh
 # echo "Deleting "
 docker rm -f galaxy-map-server
 echo "Running galaxy-map-server.."
 
-docker run --name galaxy-map-server --link data-planet:mongo  --link map-tile-server:tiles --link navi-computer-node:navcom --env NODE_ENV=development  -v /${PWD}/../://root/app  -p 8103:8103 galaxy-map-server
+# docker run --name galaxy-map-server --link data-planet:mongo  --link map-tile-server:tiles --link navi-computer-node:navcom --env NODE_ENV=development  -v /${PWD}/../://root/app  -p 8103:8103 galaxy-map-server
+
+docker run --name galaxy-map-server --link data-planet:mongo  --link map-tile-server:tiles --link navi-computer-node:navcom --env NODE_ENV=development  -v /${PWD}/../://root/app  -p 8103:8103 gmoneycool/galaxy-map-server
 
